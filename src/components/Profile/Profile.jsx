b9c084dc2a96b06695c520ba68476b20385cf9a6
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-06-19 12:38:09 +0300

    fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -44,8 +44,9 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  })
 }
